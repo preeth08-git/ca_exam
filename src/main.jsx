762badfde82efd0efb0025735d5ee1d52888bb0f
PraveenKumar <preethamkatariya@gmail.com>
2026-04-21 09:42:30 +0530

    final sub update

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { AppProvider } from "./context/AppContext";
 
+<script type="module" src="/src/main.jsx"></script>
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <AppProvider>
     <App />
