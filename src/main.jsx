45f3e0ee3fea5b9ae906a480d4a1f4e6c7d7a981
PraveenKumar <preethamkatariya@gmail.com>
2026-04-21 10:14:51 +0530

    final submits

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { AppProvider } from "./context/AppContext";
-
-<script type="module" src="/src/main.jsx"></script>
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <AppProvider>
-    <App />
-  </AppProvider>
+  <App />
 );
