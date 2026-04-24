5aa03c84df92914c82681a799c2d32dd6e9e58aa
PraveenKumar <preethamkatariya@gmail.com>
2026-04-24 14:26:26 +0530

    rename AppContext.js to AppContext.jsx

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,15 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  esbuild: {
+    include: /src\/.*\.[jt]sx?$/,
+    exclude: [],
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: {
+        '.js': 'jsx',
+      },
+    },
+  },
 })
